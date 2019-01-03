8a664b6c486e05a54d8597b8d7402e97f79b513f
YS <romantic.night.dreamer@gmail.com>
2019-01-04 00:13:14 +0300

    removed odd_or_even func
    i just now realized that i could avoid using a function just by plassing this (value % 2) instead >.>

--- CollatzV4.py
+++ CollatzV4.py
@@ -1,56 +1,36 @@
-#TODO: print at the end the highest number that occured & the number of steps it took to get to 1
-#TODO: implement a memoryList (after the script finishes it should print a list of the numbers
-#      that already occured/tested) that you can add to the same memoryList so that the next time
-#      the script is run it would ignore already tested numbers.
-# i am running in an endless loop when i add the .append code within the collatz function.
-# one idea is
-# 1: ask if you want to use the testedNumbers list before starting.
-# 2: when you encounter a tested number ask to skip or not.
+# TODO: print at the end the highest number that occured & the number of steps it took to get to 1
+# TODO:
+# 1: ask if you want to use the testedNumbers list after finishing.
+# 2: implement runMode() at the end of the first run & ask what the user want to do next
+
 print('''The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined as follows:
       start with any positive integer n. Then each term is obtained from the previous term as follows:
       if the previous term is even, the next term is one half the previous term.
       If the previous term is odd, the next term is 3 times the previous term plus 1.
       The conjecture is that no matter what value of n, the sequence will always reach 1.''')
-
 print('more information here: https://en.wikipedia.org/wiki/Collatz_conjecture')
 # https://trinket.io/python/d3bdba0162
 # https://github.com/Liyannis/Collatz
 
+
 def collatz(value):
     testedNumbersCheck(value)
     while value != 1:
-        if odd_or_even(value) == 0:
+        if (value % 2) == 0:
             value = (value // 2)
             testedNumbersCheck(value)
             print(value, 'even')
-        elif odd_or_even(value) == 1:
+        elif (value % 2) == 1:
             value = (3 * value + 1)
             testedNumbersCheck(value)
             print(value, 'odd')
         continue
 
-#TODO
-#def runMode():
-    '''ask user if he wants to use:
-       A) handsfree approach & watch the numbers,
-       B) check for already tested numbers in the testedNumbers list
-       C) run for a single number''' 
-    print('''Hello, this script supports 3 ways to run the Collatz conjecture.\n
-          in what mode do you want to run the script?\n
-          1: Full auto
-          2:
-          3: ''')
-    uC = int(input()) #userChoice
-    if uC == 1:
-        pass
-    elif uC == 2:
-        pass
-    elif uC == 3:
-        pass
-
 
 testedNumbers = []
 YES = ['y', 'Y']
+
+
 def testedNumbersCheck(value):
     if value in testedNumbers:
         print('number ' + str(value) + ' has been tested in the past')
@@ -58,26 +38,19 @@ def testedNumbersCheck(value):
         print('y/Y to stop or press Enter to continue')
         userAnswer = input('')
         if userAnswer in YES:
-            print('''if you want to rerun the script but not compute
-                     for the numbers that you already tested, \n
-                     then simply copy-paste the testedNumbers below into the code''')
+            print('''\nif you want to rerun the script but not compute
+for the numbers that you already tested,
+then simply copy-paste the testedNumbers below into the code \n''')
             print(testedNumbers)
             exit()
         else:
             pass
     else:
         testedNumbers.append(value)
-        
 
-def odd_or_even(intNumber):
-    num_test = (intNumber % 2)
-    if num_test == 2 or num_test == 0:
-        return num_test
-    elif num_test == 1:
-        return num_test
 
-#User input validation
-def userInValNum(): #userInputValidationNumber
+# User input validation
+def userInValNum():  # userInputValidationNumber
     Pass = None
     while Pass != 1:
         userIn = input('Type a positive integer number: ')
@@ -98,8 +71,40 @@ def userInValNum(): #userInputValidationNumber
                 return int(userIn)
 
 
+# TODO
+def runMode():
+    pass
+    '''ask user if he wants to use:
+       A) Auto mode where the script runs by itself,
+       B) Check for already tested numbers in the testedNumbers list
+       C) run for a single number'''
+    print('''Hello, this script supports 3 ways to run the Collatz conjecture.\n
+          in what mode do you want to run the script?\n
+          1: Full auto (it will continue indefenetlly)
+          2: Semi-auto
+          3: Manual''')
+    uC = int(input())  # userChoice
+    if uC == 1:
+        autoMode()
+    elif uC == 2:
+        pass
+    elif uC == 3:
+        pass
+
+
+def autoMode():
+    for n in testedNumbers:
+        numberOut = n + 1
+        if numberOut not in testedNumbers:
+            collatz(numberOut)
+        else:
+            pass
+        
+
 collatz(userInValNum())
-print('end')
-print('''if you want to rerun the script but not compute for the numbers that you already tested, \n
-         then simply copy-paste the testedNumbers below into the code''')
+print('Script finished')
+print('''\nif you want to rerun the script but not compute for the numbers that you already tested,
+then simply copy-paste the testedNumbers below into the code
+find this line "testedNumbers = []" and paste the numbers WITH the [] and rerun the script \n''')
 print(testedNumbers)
+runMode()
