5d7a17539a9087685d4023277a6665a9872d15f2
YS <romantic.night.dreamer@gmail.com>
2019-01-04 22:26:06 +0300

    .
    auto mode working? not sure yet

--- CollatzV4.py
+++ CollatzV4.py
@@ -10,22 +10,27 @@ print('''The Collatz conjecture is a conjecture in mathematics that concerns a s
       start with any positive integer n. Then each term is obtained from the previous term as follows:
       if the previous term is even, the next term is one half the previous term.
       If the previous term is odd, the next term is 3 times the previous term plus 1.
-      The conjecture is that no matter what value of n, the sequence will always reach 1.''')
+      The conjecture is that no matter what number of n, the sequence will always reach 1.''')
 
 print('more information here: https://en.wikipedia.org/wiki/Collatz_conjecture')
 # https://trinket.io/python/d3bdba0162
 # https://github.com/Liyannis/Collatz
 
-def collatz(value):
-    testedNumbersCheck(value)
-    while value != 1:
-        test = odd_or_even(value)
+def collatz(number):
+    while number != 1 and testedNumbersCheck(number) != 0:
+        test = odd_or_even(number)
         if test == 0:
-            value = (value // 2)
-            testedNumbersCheck(value)
+            testedNumbers.append(number)
+            print(number, 'even')
+            number = (number // 2)
+            if testedNumbersCheck(number) == 0:
+                pass
         elif test == 1:
-            value = (3 * value + 1)
-            testedNumbersCheck(value)
+            testedNumbers.append(number)
+            print(number, 'odd')
+            number = (3 * number + 1)
+            if testedNumbersCheck(number) == 0:
+                pass
         continue
 
 
@@ -33,13 +38,60 @@ def collatz(value):
 def odd_or_even(number):
     num_test = (number % 2)
     if num_test == 2 or num_test == 0:
-        print(number, 'even')
         return num_test
     elif num_test == 1:
-        print(number, 'odd')
         return num_test
     
 
+YES = ['y', 'Y']
+testedNumbers = []
+full_auto_on = 0
+def testedNumbersCheck(number):
+    if full_auto_on == 1:
+        if number in testedNumbers:
+            print('skipping ' + str(number) + ' as it has been tested')
+            escape_number = 0
+            return escape_number
+        else:
+            pass
+    elif number in testedNumbers:
+        print('number ' + str(number) + ' has been tested in the past')
+        print('Do you want to stop?')
+        print('y/Y to stop or press Enter to continue')
+        userAnswer = input('')
+        if userAnswer in YES:
+            print('''if you want to rerun the script but not compute
+                     for the numbers that you already tested, \n
+                     then simply copy-paste the testedNumbers below into the code''')
+            print(testedNumbers)
+            exit()
+        else:
+            number += 1
+            return number
+    else:
+        pass
+        
+
+#User input validation
+def userInNumVal(): #userInputNumberValidation
+    Pass = None
+    while Pass != 1:
+        userIn = input('Type a positive integer number: ')
+        try:
+            int(userIn)
+        except numberError:
+            print('oups something went wrong, try again')
+            Pass = 0
+        else:
+            number = int(userIn)
+            if number == 1:
+                print('if you input 1 the script will stop running as it outputs 1.')
+            elif number == 0:
+                print('with 0 as an input the script will be stuck in an endless loop')
+            elif number <= 0:
+                print("negative numbers end up in a never ending loop")
+            elif number >= 2:
+                return int(userIn)
 
 # TODO
 def runMode():
@@ -50,8 +102,8 @@ def runMode():
     print('''Hello, this script supports 3 ways to run the Collatz conjecture.\n
           in what mode do you want to run the script?\n
           1: Full auto
-          2:
-          3: ''')
+          2: Semi-auto?
+          3: Manual ''')
     uC = int(input()) #userChoice
     if uC == 1:
         global full_auto_on
@@ -75,57 +127,6 @@ def fullAuto():
                     pass
             else:
                 pass
-
-
-
-YES = ['y', 'Y']
-testedNumbers = []
-full_auto_on = 0
-def testedNumbersCheck(value):
-    if full_auto_on == 1:
-        if value in testedNumbers:
-            print('skipping ' + str(value) + ' as it has been tested')
-        else:
-            pass
-    elif value in testedNumbers:
-        print('number ' + str(value) + ' has been tested in the past')
-        print('Do you want to stop?')
-        print('y/Y to stop or press Enter to continue')
-        userAnswer = input('')
-        if userAnswer in YES:
-            print('''if you want to rerun the script but not compute
-                     for the numbers that you already tested, \n
-                     then simply copy-paste the testedNumbers below into the code''')
-            print(testedNumbers)
-            exit()
-        else:
-            value += 1
-            return value
-    else:
-        testedNumbers.append(value)
-        
-
-#User input validation
-def userInNumVal(): #userInputNumberValidation
-    Pass = None
-    while Pass != 1:
-        userIn = input('Type a positive integer number: ')
-        try:
-            int(userIn)
-        except ValueError:
-            print('oups something went wrong, try again')
-            Pass = 0
-        else:
-            value = int(userIn)
-            if value == 1:
-                print('if you input 1 the script will stop running as it outputs 1.')
-            elif value == 0:
-                print('with 0 as an input the script will be stuck in an endless loop')
-            elif value <= 0:
-                print("negative numbers end up in a never ending loop")
-            elif value >= 2:
-                return int(userIn)
-
 
 
 collatz(userInNumVal())
